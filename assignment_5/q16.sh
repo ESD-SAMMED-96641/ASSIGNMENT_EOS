4940ef851ea3beebe0c754421577fd0640a2b998
sammed <dhanal.sammed@gmail.com>
2026-04-15 13:22:44 +0530

    assignment

---
+++ assignment_5/q16.sh
@@ -0,0 +1,4 @@
+#Write a shell script to display welcome message to the user along with contents of his
+#home directory. Ensure that shell script will execute automatically when user login to the shell.
+#========================================================================================
+
